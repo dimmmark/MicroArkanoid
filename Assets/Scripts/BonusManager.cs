66bdd5ad52312b447a8c7d3400c218d49dede5d4
dimmmark <105634628+dimmmark@users.noreply.github.com>
2023-07-23 11:34:13 +0400

    AddBonusLogicCode

--- Assets/Scripts/BonusManager.cs
+++ Assets/Scripts/BonusManager.cs
@@ -6,17 +6,18 @@ public class BonusManager : MonoBehaviour
     [SerializeField] private Platform _platform;
     [SerializeField] private GameObject _safeLine;
     [SerializeField] private float _safeLineTimer;
+    private int _randomInt;
     //[SerializeField] private float _expandPlatformTimer;
     private bool _isOn;
     void Start()
     {
-        InvokeRepeating(nameof(SpawnBall), 5, 5);
+        // InvokeRepeating(nameof(SpawnBall), 5, 5);
     }
     private void Update()
     {
         if (_safeLineTimer > 0 && _isOn)
         {
-        _safeLineTimer -= Time.deltaTime;
+            _safeLineTimer -= Time.deltaTime;
             _safeLine.SetActive(true);
         }
         else
@@ -39,13 +40,47 @@ public class BonusManager : MonoBehaviour
     //{
     //    _platform.transform.localScale.x 
     //}
+    private void MakeBonus(Pixel pixel)
+    {
+        if (Game.Instance.BallsList.Count <= 3)
+        {
+            _randomInt = Random.Range(1, 6);
+            Debug.Log(_randomInt);
+            if (_randomInt == 2)
+            {
+                Instantiate(_bonusArray[Random.Range(0, _bonusArray.Length)], pixel.transform.position, Quaternion.identity);
+            }
+        }
+        else if (Game.Instance.BallsList.Count > 3 && Game.Instance.BallsList.Count <= 21)
+        {
+            _randomInt = Random.Range(1, 21);
+            Debug.Log(_randomInt);
+            if (_randomInt == 2)
+            {
+                Instantiate(_bonusArray[Random.Range(0, _bonusArray.Length)], pixel.transform.position, Quaternion.identity);
+            }
+        }
+        else if (Game.Instance.BallsList.Count > 21 && Game.Instance.BallsList.Count <= 100)
+        {
+            _randomInt = Random.Range(1, 51);
+            Debug.Log(_randomInt);
+            if (_randomInt == 2)
+            {
+                Instantiate(_bonusArray[Random.Range(0, _bonusArray.Length)], pixel.transform.position, Quaternion.identity);
+            }
+        }
+    }
     private void OnEnable()
     {
         BonusSafeLine.OnBonusSafeLine += SetSafeLine;
-       
+        Ball.OnCollidedPixel += MakeBonus;
+
     }
+
+
     private void OnDisable()
     {
         BonusSafeLine.OnBonusSafeLine -= SetSafeLine;
+        Ball.OnCollidedPixel -= MakeBonus;
     }
 }
